684a11e6e7c5efd678a7a5b08308726c872b40dc
halcyon-past <titanssuperior@gmail.com>
2026-04-07 03:28:51 +0530

    redesign

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,16 @@ import NewsList from './pages/NewsList'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/news" element={<NewsList />} />
-      <Route path="/news/:date" element={<DatePage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<NewsList />} />
+        <Route path="/news/:date" element={<DatePage />} />
+      </Routes>
+      <div className="site-credit">
+        This website is owned by <a href="https://aritro.cloud" target="_blank" rel="noreferrer">aritro.cloud</a>
+      </div>
+    </>
   )
 }
 
